72a183959de205ee517e066c812abc93caa0fd32
Roy Reyna <royreyna7@yahoo.com>
2020-03-15 23:02:14 -0500

    data printing to table

--- assets/app.js
+++ assets/app.js
@@ -27,11 +27,82 @@ var next_train = [];
 var minutes_away = [];
 
 // Input variables: from the form
-var train_name = "";
-var destination = "";
-var first_time = "";
+var train = "";
+var place = "";
+var time = "";
 var frequency = "";
 
 // Variables for times
 var next_arrival = "";
 var min_away = "";
+
+// Capture Button Click
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabbed values from text boxes
+    train = $("#train-name-input").val().trim();
+    train_names.push(train);
+
+    place = $("#destination-input").val().trim();
+    destinations.push(place);
+
+    time = $("#first-time-input").val().trim();
+
+    frequency = $("#frequency-input").val().trim();
+    frequencies.push(frequency);
+
+    database.ref().push({
+        train_name: train,
+        destination: place,
+        first_time: time,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+function InitializeFirebase(){
+
+    for(i=0;i<train_names.length;i++){
+
+        train = train_names[i];
+        place = destinations[i];
+        time = "";
+        frequency = frequencies[i];
+
+        database.ref().push({
+            train_name: train,
+            destination: place,
+            first_time: time,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+       }
+}
+
+database.ref().on("child_added", function(added) {
+
+
+    console.log(added.val().train_name);
+    console.log(added.val().destination);
+    console.log(added.val().first_time);
+    console.log(added.val().frequency);
+
+
+   
+    $("#train-list").append("<tr><td>" + added.val().train_name + 
+      " </td><td>" + added.val().destination +
+      " </td><td> " + added.val().frequency +
+      " </td><td>" + "" +
+      " </td><td> " + "" +
+      " </td><td>" + "" +
+      " </td></tr>");
+
+ 
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+  InitializeFirebase();
+
+  
